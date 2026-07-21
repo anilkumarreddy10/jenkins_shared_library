78094e3c5d4f65721e7bcf90814860f3b20da3da
anilkumarreddy10 <47165386+anilkumarreddy10@users.noreply.github.com>
2026-07-21 16:27:17 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,14 +1,7 @@
 def call() {
   stage('Build') {
-            steps {
-                configFileProvider([
-                    configFile(fileId: 'maven-settings', variable: 'SETTINGS')]) {
-
-                    sh """ mvn -B release:clean install \
-                    -Dmaven.test.failure.ignore=true \
-                    -s ${SETTINGS}
-                    """
-                }
-            }
+        steps {
+                sh "mvn -B release:clean install"
         }
+  }
 }
